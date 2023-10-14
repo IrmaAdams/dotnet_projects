7af0fd5253639f83487557cd54337ff25d42186e
TeddyAdams <80841726+IrmaAdams@users.noreply.github.com>
2023-10-14 13:57:11 +0100

    Created project
    C# Variable Notes

--- ToDoList/ToDoList/Program.cs
+++ ToDoList/ToDoList/Program.cs
@@ -1,2 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+Console.WriteLine("User input: " + userChoice);
+
+
 Console.ReadKey();
